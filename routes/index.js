86ef07cb7672d5f9dbe311827ce18321d92e6e9c
Allyssa <a.alimoestar@gmail.com>
2022-04-11 13:52:29 +0200

    :poop: WIP
    Co-authored-by: Hoa0 <Hoa0@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,36 @@
-const express = require("express")
+const express = require("express");
+const { graphql } = require("@octokit/graphql");
+const graphqlAuth = graphql.defaults({
+  headers: {
+    authorization: "token " + process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+  },
+});
 
 module.exports = express
-    .Router()
+  .Router()
 
-    .get("/", (req, res) => {
-        res.render("index", {
-            pageTitle: "Ayeee new project minor!"
-        })
-    })
+  .get("/", (req, res) => {
+    graphqlAuth(`{
+            user(login: "AllyssaA) {
+                repositories(affiliations: OWNER, first: 100) {
+                  edges {
+                    node {
+                        id
+                        description
+                        name
+                        pushedAt
+                        nameWithOwner
+                        isPrivate
+                    }
+                  }
+                }
+              }`)
+              
+              .then((data) => {
+                console.log(data.user.repositories.edges)
+      res.render("index", {
+        index: data.user.repositories.edges,
+      });
+    });
+
+  });
