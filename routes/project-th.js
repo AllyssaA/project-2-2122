67245fcdab3ee01ec6e85a59cdfb8a5494a7bbda
Allyssa <a.alimoestar@gmail.com>
2022-04-14 12:37:17 +0200

    :sparkles: added frontpage, added links

---
+++ routes/project-th.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { graphql } = require("@octokit/graphql");
+const graphqlAuth = graphql.defaults({
+  headers: {
+    authorization: "token " + process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+  },
+});
+
+module.exports = express.Router().get("/", (req, res) => {
+  graphqlAuth(`{
+      user(login: "Hoa0") {
+        repositories(affiliations: OWNER, first: 100, privacy: PUBLIC, orderBy: {field: UPDATED_AT, direction: DESC}, isFork: false) {
+          edges {
+            node {
+              name
+              description
+              pushedAt
+              url
+              homepageUrl
+              openGraphImageUrl
+            }
+          }
+        }
+      }
+    }`).then((data) => {
+    // console.log(data.login.repositories.edges)
+    res.render("project-th", {
+      projects: data.user.repositories.edges,
+      pageTitle: "Github Portfolio",
+    });
+  });
+});
